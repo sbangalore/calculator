e26c84cf89444771f1e2dc6159638f869e07485b
Sudarshan Bangalore <bsudarshan@outlook.com>
2018-03-20 10:57:30 -0400

    updates to logic

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,11 +1,47 @@
+var command;
+var result;
+
 var result = 0;
-var a = '';
-var b = '';
+var a = 0;
+var b = 0;
+
+document.addEventListener('keypress', function() {
+  if (event.key == "Backspace" || event.key == "Delete") {
+    console.log('hi');
+  }
+  else if (event.keyCode == 43) {
+    operate(add(a,b));
+  }
+  else if (event.keyCode == 49) {
+    input(1);
+  }
+  else if (event.keyCode == 50) {
+    input(2);
+  }
+  else if (event.keyCode == 51) {
+    input(3);
+  }
+  else if (event.keyCode == 52) {
+    input(4);
+  }
+  else if (event.keyCode == 53) {
+    input(5);
+  }
+  else if (event.keyCode == 54) {
+    input(6);
+  }
+  else if (event.keyCode == 55) {
+    input(7);
+  }
+  else if (event.keyCode == 56) {
+    input(8);
+  }
+  else if (event.keyCode == 57) {
+    input(9);
+  }
+});
 
 
-function add(a,b) {
-  return a+b;
-}
 
 function subtract(a,b) {
   return a-b;
@@ -23,8 +59,31 @@ function inverse(a) {
   return a*-1;
 }
 
-function operate(operator, a) {
-  return operator(a, b);
+function add(a,b) {
+  return a+b;
+}
+
+function operate(operator) {
+  if (command != '') {
+    equals();
+  }
+  result = a;
+  command = operator;
+  console.log(command);
+  a=0;
+}
+
+function equals() {
+  switch (command) {
+    case add:
+      a = add(a,result);
+      break;
+    case inverse:
+      a = inverse(result);
+    default:
+      break;
+  }
+  display(a);
 }
 
 function input(playerInput) {
@@ -36,21 +95,14 @@ function input(playerInput) {
 
 function display(result) {
   document.getElementById('result').innerHTML=result;
-  if (result>0) {
-    document.getElementById('clearButton').innerHTML='C';
-  }
-  else if (result==0 || result=="") {
-    document.getElementById('clearButton').innerHTML='AC';
-  }
 }
 
-function backspace() {
+function undo() {
   a = Math.floor(a/10);
-  if (a!=0){
+  if (a!=0) {
     display(a);
   }
   else {
-    a = '';
     display(a);
   }
 }
